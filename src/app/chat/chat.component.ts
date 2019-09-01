463571158d12543d4cae9bc4dd3b479d7d2bc1a9
seyeadama03 <seyeadama03@outlook.com>
2019-09-01 20:18:49 +0000

    chat

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -19,7 +19,7 @@ export class ChatComponent implements OnInit {
   ngOnInit() {
   }
 
-  private serverUrl = 'http://localhost:8083/socket';
+  private serverUrl = 'http://localhost:8888/socket';
   private title = 'WebSockets chat';
   private stompClient;
 
